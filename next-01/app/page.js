1e5e511145dec01428fa92fee5c7b7b49e94c364
Yasmin Marcelly <yasminmarcelly11@gmail.com>
2026-02-20 00:29:48 +0000

    primeiro componente react

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import Profile from '../components/Profile';
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
         The truth is out there.
+        <Profile />
       </h1>
     </div>
   );
---
+++ next-01/components/Profile.js
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/r4rCTg9.jpeg"
+      alt="Katherine Johnson"
+    />
+  )
+}
